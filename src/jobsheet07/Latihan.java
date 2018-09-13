0fbb57fac8296efc71bbeae07d3b5dd8c246b1ed
- INDIEGLO - <- INDIEGLO -@widya>
2018-09-13 13:50:14 +0700

    Tambah Hitung Fibonacci

--- src/jobsheet07/Latihan.java
+++ src/jobsheet07/Latihan.java
@@ -14,6 +14,7 @@ public class Latihan {
         
         tampilJudul(identitas);
         int n = tampilInput();
+         BigInteger hasil = fibo(n);
     }
      private static int tampilInput() 
     {
@@ -23,5 +24,18 @@ public class Latihan {
         int n = scanner.nextInt();
         
         return n;
+    }
+      private static BigInteger fibo(int n)
+    {
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for(int i = 2; i < n; i++)
+        {
+             hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+             return hasil[n-1];
     }
 }
