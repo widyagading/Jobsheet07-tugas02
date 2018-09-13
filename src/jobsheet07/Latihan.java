edc5764464ef544450455123564744aa6f6973a3
- INDIEGLO - <- INDIEGLO -@widya>
2018-09-13 13:51:34 +0700

    Tambah Tampil Hasil

--- src/jobsheet07/Latihan.java
+++ src/jobsheet07/Latihan.java
@@ -15,6 +15,7 @@ public class Latihan {
         tampilJudul(identitas);
         int n = tampilInput();
          BigInteger hasil = fibo(n);
+         tampilHasil(n, hasil);
     }
      private static int tampilInput() 
     {
@@ -37,5 +38,9 @@ public class Latihan {
              hasil[i] = hasil[i-1].add(hasil[i-2]);
         }
              return hasil[n-1];
+    }
+      private static void tampilHasil(int n, BigInteger hasil)
+    {
+        System.out.println("Bilangan Fibonacci ke-" + n + " : " + hasil);
     }
 }
