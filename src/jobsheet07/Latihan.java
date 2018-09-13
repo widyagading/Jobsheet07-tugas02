ce8934bb497f10e728e7764f22c23543f440c61e
- INDIEGLO - <- INDIEGLO -@widya>
2018-09-13 13:47:48 +0700

    Initial Commit

---
+++ src/jobsheet07/Latihan.java
@@ -0,0 +1,17 @@
+package jobsheet07;
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Latihan {
+    public static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas: " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    public static void main(String[] args) {
+        String identitas = "Widya Gading Sejati / XRPL2 / 38";
+        
+        tampilJudul(identitas);
+    }
+}
---
+++ src/jobsheet07/tugas01.java
@@ -0,0 +1,11 @@
+
+package jobsheet07;
+import java.util.Scanner;
+public class tugas01 {
+    public static void main(String[] args)
+    {
+        int[] nominal = {100, 500, 1000, 2000, 5000};
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
