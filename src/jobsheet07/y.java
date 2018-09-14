b55c95079eaf92c6aa0f5917342f18bb770d8dd5
- INDIEGLO - <- INDIEGLO -@widya>
2018-09-14 21:08:37 +0700

    Deklarasi

--- src/jobsheet07/y.java
+++ src/jobsheet07/y.java
@@ -5,3 +5,12 @@ public class Tugas02 {
     {
         System.out.println("Identitas: " + identitas);
     }    
+public static void main(String[] args)
+    {
+        
+        Scanner sistem = new Scanner(System.in);
+        int arr[] = {4, 5, 2, 3, 7};
+        int arr1[] = {5000, 7000, 2000, 3000, 1000};
+        int bayaran = 0;
+        String identitas = "Widya Gading Sejati / XRPL2 / 38";        
+        tampilJudul(identitas);
