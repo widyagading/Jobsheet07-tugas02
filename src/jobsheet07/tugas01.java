1d2a7a785349505359343582bd067d54e9f763ad
- INDIEGLO - <- INDIEGLO -@widya>
2018-09-14 21:01:23 +0700

    Proses Output

--- src/jobsheet07/tugas01.java
+++ src/jobsheet07/tugas01.java
@@ -13,4 +13,18 @@ public class tugas1 {
         int[] nominal = {3000, 2000, 1000, 500};
         Scanner scan = new Scanner(System.in);
         Scanner baca = new Scanner(System.in);
+        
+        System.out.print("Uang: ");
+        int uang = baca.nextInt();
+        
+        for(int i = 0; i < nominal.length; i++)
+        {
+            int jumlah = uang / nominal[i];
+            uang = uang - (nominal[i] * jumlah);
+            
+            System.out.println("Lembar " + nominal[i] + " = " + jumlah);
+        }
+        
+    }
+}
 }
